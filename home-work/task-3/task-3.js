16ab19f6c8ada515188166fc965957eeec619838
Yuliia Kushnyrenko <kushnyrenkouliia8gmail.com>
2020-05-19 16:19:35 +0300

    Add changed

---
+++ home-work/task-3/task-3.js
@@ -0,0 +1,15 @@
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
